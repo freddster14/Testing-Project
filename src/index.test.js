bdb1e8c218aba01ac8d8ab16850e3e6f96ce3cac
Zeronicc <zeronic4465@gmail.com>
2024-06-24 17:24:58 -0500

    Add tests to Analyze Array

--- src/index.test.js
+++ src/index.test.js
@@ -36,7 +36,6 @@ test('shifts string by num amount return encrypted string', () => {
     expect(caesarCipher('Yoshi', 10)).toBe('Iycrs');
     expect(caesarCipher('Y^o%&*', 5)).toBe('D^t%&*');
     expect(caesarCipher(209, 2)).toBe('input is a number')
-
 })
 
 test('array of numbers returns average, min, max, and length' , () => {
@@ -44,5 +43,26 @@ test('array of numbers returns average, min, max, and length' , () => {
     expect(object.average).toBe(4);
     expect(object.min).toBe(1);
     expect(object.max).toBe(8);
+    expect(object.length).toBe(6);
+})
+test('array of numbers with decimal returns average, min, max, and length' , () => {
+    const object = analyzeArray([5,8,10,21,33,894,0.5])
+    expect(object.average).toBeCloseTo(138.85);
+    expect(object.min).toBe(0.5);
+    expect(object.max).toBe(894);
+    expect(object.length).toBe(7);
+})
+test('array of numbers returns errors' , () => {
+    const object = analyzeArray([-1,'four',10,2,2,60])
+    expect(object.average).toBe(10);
+    expect(object.min).toBe(-21);
+    expect(object.max).toBe(60);
+    expect(object.length).toBe(6);
+})
+test('array of negative numbers returns average, min, max, and length' , () => {
+    const object = analyzeArray(['21',22,-542,-10,'1','0'])
+    expect(object.average).toBeCloseTo(-84.66);
+    expect(object.min).toBe(-542);
+    expect(object.max).toBe(22);
     expect(object.length).toBe(6);
 })

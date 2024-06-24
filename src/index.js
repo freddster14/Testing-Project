6fcb0f87d7bc009581096d05afdd61901af3c155
Zeronicc <zeronic4465@gmail.com>
2024-06-24 15:24:13 -0500

    Implemented test cases and their function
    Improving my TDD skill

--- src/index.js
+++ src/index.js
@@ -1,4 +1,75 @@
-export default function sum(a, b) {
-    return a + b
+
+function capitalize(string) {
+    const firstChar = string.at(0).toUpperCase()
+    const notFirstChar = string.slice(1, string.length)
+    return firstChar + notFirstChar
 }
 
+function reverseString(string) {
+    let length = string.length;
+    let reversed = '';
+    for(let n in string){
+        length = length -= 1
+        reversed += string[length]
+    }
+    return reversed
+} 
+class Calculator{
+    add(a, b){
+        return a + b
+    }
+    subtract(a, b){
+        return a - b
+    }
+    divide(a, b){
+        return a / b
+    }
+    multiply(a, b){
+        return a * b
+    }
+}
+function caesarCipher(string, shift) {
+    const alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m',
+                    'n','o','p','q','r','s','t','u','v','w','x','y','z'];
+    let encrypted = '';
+    for(let n in string){
+        //Pushes none characters
+        if(!string[n].match(/[a-zA-Z]/g)) {
+            encrypted += string[n]
+            continue;
+        }
+        let index = alphabet.indexOf(string[n].toLowerCase())
+        index += shift;
+        if(index > 25) index -= 26;
+        //Checks UpperCase
+        if(string[n] === string[n].toUpperCase() && string[n] !== string[n].toLowerCase()){
+            encrypted += alphabet[index].toUpperCase()
+        }else{ encrypted += alphabet[index]}
+    }
+    return encrypted
+}
+function analyzeArray(array) {
+        let object = {};
+
+        let total = 0;
+        for(let n in array){
+            total += array[n]
+        }
+        object.average = total / array.length;
+    
+        let min = array[0];
+        for(let n in array){
+            if(min > array[n]) min = array[n]
+        }
+        object.min = min;
+
+        let max = array[0]
+        for(let n in array){
+            if(max < array[n]) max = array[n]
+        }
+        object.max = max;
+        object.length = array.length;
+        return object
+}
+
+export {capitalize, reverseString, Calculator, caesarCipher, analyzeArray};
--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,33 @@
-import sum from './index'
+import {capitalize, reverseString, Calculator, caesarCipher, analyzeArray} from './index'
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+test('strings first character capitalized', () => {
+    expect(capitalize('hello world')).toBe('Hello world');
+    expect(capitalize('jest')).toBe('Jest');
+});
+
+test('reverses the string' , () => {
+    expect(reverseString('snoops')).toBe('spoons');
+    expect(reverseString('like')).toBe('ekil');
+});
+
+test('calculates return result', () => {
+    let calculator = new Calculator;
+    expect(calculator.add(4, 10)).toBe(14);
+    expect(calculator.subtract(67, 27)).toBe(40);
+    expect(calculator.divide(115, 5)).toBe(23);
+    expect(calculator.multiply(33, 4)).toBe(132);
+});
+
+test('shifts string by num amount return encrypted string', () => {
+    expect(caesarCipher('xyz', 3)).toBe('abc');
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr');
+    expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!');
+})
+
+test('array of numbers returns average, min, max, and length' , () => {
+    const object = analyzeArray([1,8,3,4,2,6])
+    expect(object.average).toBe(4);
+    expect(object.min).toBe(1);
+    expect(object.max).toBe(8);
+    expect(object.length).toBe(6);
 })
